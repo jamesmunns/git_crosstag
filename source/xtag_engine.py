647e1ade91cfffc0034a14f235a77e18ec20c1f4
James Munns <james.munns@gmail.com>
2016-01-25 23:12:47 +0100

    Actually generate the example markdown file

--- source/xtag_engine.py
+++ source/xtag_engine.py
@@ -16,6 +16,11 @@ def get_project_name():
 
 
 def write_metadata(ofile, metadata):
+
+    # TODO: yaml doesnt know what to do with a namedtuple
+    for key in metadata:
+        metadata[key] = dict(metadata[key]._asdict())
+
     with open(ofile, 'w') as yaml_file:
         yaml_file.write(yaml.dump(metadata,
                                   default_flow_style=False))
@@ -24,6 +29,6 @@ if __name__ == '__main__':
 
     xtags = parse.render('../examples/unmarked/marketing-requirements.md')
 
-    #write_metadata(xtags)
+    write_metadata('../examples/unmarked/marketing-requirements.md.xtag', xtags)
     for tag in xtags:
         print(tag, xtags[tag])
